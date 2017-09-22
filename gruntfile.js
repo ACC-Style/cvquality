fec2f8a287e1334128217ec54ae09c5b8470459e
Matt Watier <mattwatier@gmail.com>
2017-09-22 14:51:55 -0400

    Moving Repos
    Cleaning up the assets so it links properly in the git page

--- gruntfile.js
+++ gruntfile.js
@@ -1,29 +1,37 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: 'assets/scss/',
-					src: ['*.scss','**/*.scss'],
-					dest: 'assets/css/',
-					ext: '.css'
-				}]
-			}
-		},
-		exec: {
-  echo_something: 'npm run styleguide'
-},
-		watch: {
-			css: {
-				files: 'assets/scss/**/*.scss',
-				tasks: ['sass','exec']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-exec');
-	grunt.registerTask('default',['watch']);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/scss/',
+                    src: ['*.scss', '**/*.scss'],
+                    dest: 'assets/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                src: 'assets/css/*',
+                dest: 'docs/assets/css/',
+            },
+        },
+        exec: {
+            echo_something: 'npm run styleguide'
+        },
+        watch: {
+            css: {
+                files: 'assets/scss/**/*.scss',
+                tasks: ['sass', 'exec']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', ['watch']);
 }
